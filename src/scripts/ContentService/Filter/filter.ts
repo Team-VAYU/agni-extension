cb5c694d39b6cbec2890f1bd33f5d05cb028b261
Harshit Kathuria <harshitkathuria14@gmail.com>
2023-03-13 02:14:28 +0530

    Fix: Hide image

--- src/scripts/ContentService/Filter/filter.ts
+++ src/scripts/ContentService/Filter/filter.ts
@@ -2,20 +2,23 @@ import {DOMImagesResponse, ImageContaintingElementType} from 'types'
 
 const imageMap = new Map()
 
-const hideElement = (element: ImageContaintingElementType) => {
-  element.style.visibility = 'hidden'
+const hideElement = (element: ImageContaintingElementType, isBlur: boolean) => {
+  if (isBlur) element.style.filter = 'blur(30px)'
+  else element.style.visibility = 'hidden'
 }
 
 const showElement = (element: ImageContaintingElementType) => {
   element.style.visibility = 'visible'
+  element.style.filter = 'none'
 }
 
 const getImageSource = (element: ImageContaintingElementType) => {
   if (element instanceof HTMLImageElement) {
-    return element.src
+    return element.currentSrc
   }
   if (element instanceof HTMLDivElement) {
-    return element.style.backgroundImage
+    const urlSrc = element.style.backgroundImage
+    return urlSrc.substring(5, urlSrc.length - 2)
   }
   if (element instanceof HTMLVideoElement) {
     return element.poster
@@ -23,14 +26,22 @@ const getImageSource = (element: ImageContaintingElementType) => {
   return null
 }
 
+const url = 'https://octopus-app-dyhid.ondigitalocean.app/'
+
 const computeImagePredictionScore = async (imageSource: string) => {
   try {
-    const res = await fetch(`/?url=${imageSource}`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
+    const res = await fetch(
+      'http://localhost:5000/?' +
+        new URLSearchParams({
+          url: imageSource,
+        }),
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
       },
-    })
+    )
     const data = await res.json()
     if (data.error_code) {
       throw new Error(data.error_reason)
@@ -38,7 +49,6 @@ const computeImagePredictionScore = async (imageSource: string) => {
       return data.score
     }
   } catch (e) {
-    console.log(e)
     throw new Error('Something went wrong')
   }
 }
@@ -48,23 +58,27 @@ export const checkElements = async (imageContainingElements: DOMImagesResponse) 
     const elements = imageContainingElements[key]
     for (const element of elements) {
       const imageSource = getImageSource(element)
+      console.log('imageSource', imageSource)
+      if (!imageSource) continue
 
-      if (!imageSource) return null
-
-      hideElement(element)
+      hideElement(element, true)
       if (!imageMap.has(imageSource)) {
         try {
           console.log(element)
           const score = await computeImagePredictionScore(imageSource)
+          // predict random score between 0 and 1
+          // const score = Math.random()
           console.log(score, imageSource, element)
           imageMap.set(imageSource, score)
         } catch (e) {
-          return null
+          console.log(e)
         }
       }
-      const score = imageMap.get(imageSource)
-      if (score < 0.1) {
-        showElement(element)
+      if (imageMap.has(imageSource)) {
+        const score = imageMap.get(imageSource)
+        if (score < 0.1) {
+          showElement(element)
+        }
       }
     }
   }
